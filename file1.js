7ab36d65502db69021479d4dbff5d7151e44ce28
37hashiguchi <tgif3773@gmail.com>
2021-02-04 05:00:00 +0900

    white evorx ver

--- file1.js
+++ file1.js
@@ -1,31 +1,32 @@
+console.log("hello"); //※反映されているかテスト
 
-    console.log("hello"); //※反映されているかテストしたら反映されていました
+var
+cursor = $(".cursor"),
+follower = $(".follower");
+cWidth = 8,
+fWidth = 40,
+delay = 10,
+mouseX = 0,
+mouseY = 0,
+posX = 0;
+posY = 0;
 
-var mouseX=window.innerWidth/2,
-mouseY=window.innerHeight/2;
-var circle = { 
-     el:$('#circle'),
-     x:window.innerWidth/2, 
-     y:window.innerHeight/2, 
-     w:80, 
-     h:80, 
-     update:function(){
-                   l = this.x-this.w/2; 
-                   t = this.y-this.h/2; 
-                   this.el.css({ 
-                            'transform':
-                            'translate3d('+l+'px,'+t+'px, 0)' }); 
-                           }
-}
-
-setInterval (move,1000/60)
-function move(){
-  circle.x = lerp (circle.x, mouseX, 0.1);
-  circle.y = lerp (circle.y, mouseY, 0.1);
-  circle.update() 
-}
-function lerp (start, end, amt){
-  return (1-amt)*start+amt*end
-}
-
-
+TweenMax.to({}, .001,{
+  repeat: -1,
+  onRepeat: function(){
+    posX += (mouseX - posX) / delay;
+    posY += (mouseY - posY) / delay;
+  TweenMax.set(follower,{
+    css:{
+      left : posX - (fWidth / 2),
+      top : posY - (fWidth/2) 
+    }
+  });
+  TweenMax.set(cursor),{
+    css:{
+      left: mouseX - (cWidth / 2),
+      top : mouseY - (cWidth / 2)
+    }
+  }
+  }
+});
